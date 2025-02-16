bf922290b5b3fe7aad65d4c097956d8b9ae1d035
Jugal Kishore <jugalchaaras@gmail.com>
2025-02-16 18:33:25 +0530

    All change

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+provider "azure" {
+    region = "us-east-2"  # Set your desired AWS region
 }
-resource "aws_instance" "JAC" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+resource "aws_instance" "Vihan" {
+    ami           = "ami-0c55b159cbfafe1f0-Vihan"  # Specify an appropriate AMI ID
+    instance_type = "t2.micro-vihan"
 }
